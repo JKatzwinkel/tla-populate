9d7adde878c90d7b2d5e5dee5b214be8dca9c3e6
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2020-04-07 21:10:09 +0200

    wait for backend connection before trying to ingest

--- tla_ingest/populate_tla.py
+++ tla_ingest/populate_tla.py
@@ -1,4 +1,9 @@
+import requests
+import time
+
 from tla_ingest import populate_from_dir
+
+BACKEND_URL = 'http://localhost:8090'
 
 TYPE_MAPPINGS = {
     'lemma': 'lemma',
@@ -7,12 +12,23 @@ TYPE_MAPPINGS = {
     'ths': 'ths',
 }
 
+def wait_for_connection(url: str):
+    connected = False
+    while not(connected):
+        try:
+            res = requests.get(url)
+            connected = True
+        except:
+            print(f'waiting for connection at {url}...')
+            time.sleep(4)
 
 def main():
+    wait_for_connection(BACKEND_URL)
     for src_dir, url_path in TYPE_MAPPINGS.items():
+        print(f'ingest {src_dir} documents')
         populate_from_dir(
             f'corpus/sample/{src_dir}',
             url_path,
-            'http://localhost:8090'
+            BACKEND_URL
         )
 
